045065dc6688495aa557bd13344fcd0f03ccd3e2
Костя Исупов <kkopylova767@gmail.com>
2025-10-30 00:30:47 +0600

    feat: add controllers

---
+++ note-app/app/Http/Controllers/GoodbyeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class GoodbyeController extends Controller
+{
+
+    public function goodbye()
+    {
+        return view('Goodbye');
+    }
+}
